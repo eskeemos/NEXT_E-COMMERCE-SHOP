d7891f498b9e83ff04a1120d5ca0840b9978f8b3
eskeemos <l.michalik004@gmail.com>
2022-12-28 11:40:29 +0100

    stripe integrate

--- lib/getStripe.jsx
+++ lib/getStripe.jsx
@@ -4,7 +4,7 @@ let stripePromise;
 
 const getStripe = () => {
   if(!stripePromise){
-    stripePromise = loadStripe("'"+process.env.STRIPE_PUBLIC_KEY+"'");
+    stripePromise = loadStripe('pk_test_51MJbQuF9guTtw2YGMb2Vs9aUZsWahXqUW6vKp3GaGpS4wglhQ4RjX0ys5jXh2gggSm8sj384BSbbL3TUdswfFg7p00ov5imo5j');
   }
 
   return stripePromise;
